15403729bcac84188aa823d42852c56db3c4710c
cabal82 <fabriziogallici@gmail.com>
2019-11-11 19:17:00 +0100

    add response

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,13 +13,29 @@ function sendMsg() {
   $('.chat-msg').val('');
 }
 
+function receivedMsg() {
+  var $message = 'ciao';
+  var $elementMsg = $('#template .received-msg').clone();
+  $elementMsg.find('.sent-text').text($message);
+  $('.chat-panel.is-active').append($elementMsg);
+  $('.chat-msg').val('');
+}
+
 $(document).ready(function () {
   //send msg on button click
-  $('.send-msg').click(sendMsg);
+  // $('.send-msg').click(sendMsg);
+  $('.send-msg').click(function() {
+    sendMsg();
+    receivedMsg();
+  })
+
+
+
   //send msg press Enter
   $(document).keypress(function (e) {
     if(e.keyCode === 13) {
       sendMsg();
+      receivedMsg();
     }
   });
 
