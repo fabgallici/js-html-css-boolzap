38d85e128876c30c7fae249aceac2d0e401d2660
cabal82 <fabriziogallici@gmail.com>
2019-11-11 15:41:14 +0100

    chat work

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
 
+  $('.send-msg').click(function() {
+
+    var $message = $('.chat-msg').val();
+
+    var $elementMsg = $('#template .sent-msg').clone();
+
+    $elementMsg.find('.sent-text').text($message);
+
+    $('.current-chat').append($elementMsg);
+
+    $('.chat-msg').val('');
+  })
 });
