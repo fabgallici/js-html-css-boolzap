09700ea89a1c2b26071a4f065bfe0927294f0880
cabal82 <fabriziogallici@gmail.com>
2019-11-11 17:22:10 +0100

    step change user

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
 
     $elementMsg.find('.sent-text').text($message);
 
-    $('.chat-panel').append($elementMsg);
+    $('.chat-panel.is-active').append($elementMsg);
 
     $('.chat-msg').val('');
   })
@@ -17,6 +17,18 @@ $(document).ready(function () {
   $('.user-panel').click(function() {
     $('.user-panel').removeClass('is-active');
     $(this).addClass('is-active');
+
+    //select data-name corresponding chat to active
+    $currentDataName = $(this).attr('data-name');
+    console.log($currentDataName);
+    $('.chat-panel').removeClass('is-active');
+    // $('.chat-panel').attr("data-name='" + $currentDataName + "'").addClass(is-active);
+    $(".chat-panel[data-name=' + $currentDataName + ']").addClass('is-active');
+    // var $currentChat = $('.chat-panel').attr('data-name', $currentDataName);
+    // console.log($currentChat);
+    // if ($('.chat-panel').attr('data-name', $currentDataName)) {
+    //   console.log('found');
+    // }
   })
 
 });
