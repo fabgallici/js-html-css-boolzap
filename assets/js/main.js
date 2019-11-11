9c742d1228b6928bbb448c3a4e4a2f3eeb29843d
cabal82 <fabriziogallici@gmail.com>
2019-11-11 18:15:09 +0100

    send func

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,18 @@
+function sendMsg() {
+  var $message = $('.chat-msg').val();
+
+  var $elementMsg = $('#template .sent-msg').clone();
+
+  $elementMsg.find('.sent-text').text($message);
+
+  $('.chat-panel.is-active').append($elementMsg);
+
+  $('.chat-msg').val('');
+}
+
 $(document).ready(function () {
 
-  $('.send-msg').click(function() {
-
-    var $message = $('.chat-msg').val();
-
-    var $elementMsg = $('#template .sent-msg').clone();
-
-    $elementMsg.find('.sent-text').text($message);
-
-    $('.chat-panel.is-active').append($elementMsg);
-
-    $('.chat-msg').val('');
-  })
+  $('.send-msg').click(sendMsg);
 
   //User panel listener
   $('.user-panel').click(function() {
@@ -22,13 +23,9 @@ $(document).ready(function () {
     $currentDataName = $(this).attr('data-name');
     console.log($currentDataName);
     $('.chat-panel').removeClass('is-active');
-    // $('.chat-panel').attr("data-name='" + $currentDataName + "'").addClass(is-active);
+  
     $('.chat-panel[data-name="' + $currentDataName + '"]').addClass('is-active');
-    // var $currentChat = $('.chat-panel').attr('data-name', $currentDataName);
-    // console.log($currentChat);
-    // if ($('.chat-panel').attr('data-name', $currentDataName)) {
-    //   console.log('found');
-    // }
+
   })
 
 });
